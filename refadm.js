e5d0b9addecb6f387153fa2480ba5f6f55193669
Instituição Fiscal Independente <56411895+ifibrasil@users.noreply.github.com>
2021-04-09 12:18:10 -0300

    Update refadm.js

--- refadm.js
+++ refadm.js
@@ -4,29 +4,15 @@ Dygraph.Plugins.SelectSeries = (function(){
 
 	var boxes = function(){this.div_ = null;};
 
-	var largura = $(window).width();
-	$(window).on('resize', function() {
-	if ($(this).width() !== largura) {
-		largura = $(this).width();
-	}
-	});
-
 	boxes.prototype.activate = function(dygraph){
-		
 
 		boxes.div_ = document.createElement("div");
-
-		if (largura < 1050) {
-			boxes.div_.style = 'width:100%;text-align:left;float:none;margin-top:80px;margin-left:0px';
-		}
-		else {
-			boxes.div_.style = 'width:260px;text-align:left;float:right;margin-top:80px;margin-left:0px';
-		}
+		boxes.div_.style = 'width:260px;text-align:left;float:right;margin-top:80px;margin-left:0px';
 
 		var colunas = dygraph.getLabels();
 		colunas.shift();
 
-		colunas.forEach(function(value){
+		colunas.forEach(function(value){ 
 			boxes.div_.innerHTML += '<p style="font-size:16px;margin-bottom:5px;font-family:Cambria;text-align:left"><input type="checkbox" checked=""> ' + value + '</p>';
 		});		
 
@@ -41,4 +27,3 @@ Dygraph.Plugins.SelectSeries = (function(){
 	return boxes;
 
 })();
-
